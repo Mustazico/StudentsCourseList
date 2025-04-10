009f7d1952470b872ea365bbb05d17a939694336
Eirik Brakstad <eirik.brakstad@hotmail.com>
2025-04-10 21:25:34 +0200

    Add project files.

---
+++ Assignment3/Courses.xaml.cs
@@ -0,0 +1,71 @@
+﻿using Assignment3.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace Assignment3
+{
+    public partial class Courses : Window
+    {
+        private readonly Dat154Context dx = new();
+
+        private readonly LocalView<Course> CoursesList;
+        private readonly LocalView<Student> Students;
+        private readonly LocalView<Grade> Grades;
+
+        public Courses(Dat154Context context)
+
+
+        {
+            InitializeComponent();
+            DataContext = this;
+            Grades = dx.Grades.Local;
+            Students = dx.Students.Local;
+            CoursesList = dx.Courses.Local;
+            studentGradesList.ItemsSource = CoursesList;
+
+            dx.Courses.Load();
+           
+            
+            
+            
+
+        }
+
+        private void studentGradesList_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (studentGradesList.SelectedItem != null)
+            {
+                Course selectedCourse = (Course)studentGradesList.SelectedItem;
+
+                var studentGradesForCourse = from grade in dx.Grades
+                                             join student in dx.Students on grade.Studentid equals student.Id
+                                             where grade.Coursecode == selectedCourse.Coursecode
+                                             select new
+                                             {
+                                                 StudentName = student.Studentname,
+                                                 Grade = grade.Grade1
+                                             };
+
+                studentGradesList.ItemsSource = studentGradesForCourse.ToList();
+            }
+        }
+
+
+
+
+    }
+}
---
+++ Assignment3/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Assignment3.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Assignment3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+
+        private readonly Dat154Context dx = new();
+        // Local cashe av data
+        private readonly LocalView<Student> Students;
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Students = dx.Students.Local;
+
+            // Vise en liste av alle studenter fra databasen
+            dx.Students.Load();
+            // Setter datacontext, hvor den skal få dataene sine fra
+            studentList.DataContext = Students.OrderBy(s => s.Studentname);
+
+        }
+
+        private void searchButton_Click(object sender, RoutedEventArgs e)
+        {
+            studentList.DataContext = Students.Where(s => s.Studentname.Contains(searchField.Text))
+                .OrderBy(s => s.Studentname);
+
+        }
+
+        private void toCourses_Click(object sender, RoutedEventArgs e)
+        {
+            Courses cw = new Courses(dx);
+            cw.Show();
+            
+        }
+    }
+}
